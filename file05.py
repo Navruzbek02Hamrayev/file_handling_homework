d0f83d32134161225a2ade7c2bd5f3b76b4d5be1
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-25 14:39:53 -0400

    Return the [len(l),len(l1)]

--- file05.py
+++ file05.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    l=[]
+    l1=[]
+    f=open(data).read()
+    for i in f:
+        if i.isdigit():
+            l.append(str(i))
+    for i in f:
+        for a in i:
+            if not i.isdigit():
+                l1.append(str(i))
+    return [len(l),len(l1)]
 # Read data from file
+print(main("data/data05.txt"))
