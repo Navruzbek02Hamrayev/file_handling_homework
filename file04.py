0e33c8a11cb430134b2a1bf8a68ee37223172c26
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-25 14:03:37 -0400

    Return the not digit

--- file04.py
+++ file04.py
@@ -6,5 +6,12 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
+    l=[]
+    f=open(data).read()
+    for i in f:
+        for a in i:
+            if not i.isdigit():
+                l.append(str(i))
+    return l
 # Read data from file
+print(main("data/data04.txt"))
