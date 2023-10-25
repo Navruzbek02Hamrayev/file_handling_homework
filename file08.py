36a77d476344b9affd4e4d253d6158e3ffc7ef67
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-25 16:24:23 -0400

    Return the max

--- file08.py
+++ file08.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    l=[]
+    f=open(data).read()
+    for i in f:
+        if i.isdigit():
+            l.append(int(i))
+            max=l[0]
+            s=0
+            while s<len(l):
+                if max<l[s]:
+                    max=l[s]
+                s+=1
+    return max
 # Read data from file
+print(main("data/data08.txt"))
