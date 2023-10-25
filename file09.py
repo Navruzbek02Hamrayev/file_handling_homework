f14d9e2b16b31b43e58de8742dc876d11f04f8bf
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-25 16:34:39 -0400

    Return the min

--- file09.py
+++ file09.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-
+    l=[]
+    f=open(data).read()
+    for i in f:
+        if i.isdigit():
+            l.append(int(i))
+            s=0
+            min=l[0]
+            while s<len(l):
+                if min>l[s]:
+                    min=l[s]
+                s+=1
+    return min
 # Read data from file
+print(main("data/data09.txt"))
