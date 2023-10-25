9fe293caa2ece3eb99b3ad7afac66bfdc6a2626f
Navruzbek02Hamrayev <hamrayevnavruz12@gmail.com>
2023-10-25 15:23:54 -0400

    Return the sum

--- file07.py
+++ file07.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    l=[]
+    f=open(data).read()
+    for i in f:
+        if i.isdigit():
+            l.append(int(i))
+            s=0
+            sum=l[0]
+            while s<len(l):
+                sum+=l[s]
+                s+=1
+    return sum
 # Read data from file
+print(main("data/data07.txt"))
